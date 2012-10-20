c040604dea76ca02144ea09e8e80accbb3442d8b
Eka A. Kurniawan <eka.a.kurniawan@gmail.com>
2012-10-21 00:35:10 +0800

    Adding Algorithm Information

--- DP_GlobalAlignment/DP_GlobalAlignment.py
+++ DP_GlobalAlignment/DP_GlobalAlignment.py
@@ -15,6 +15,8 @@
 # Free Software Foundation, Inc.,
 # 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
+# Global Alignment - Dynamic Programming
+
 import copy
 
 # Seq1 = "AAGC"
--- DnC_GlobalAlignment/DnC_GlobalAlignment.py
+++ DnC_GlobalAlignment/DnC_GlobalAlignment.py
@@ -15,6 +15,8 @@
 # Free Software Foundation, Inc.,
 # 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
+# Global Alignment in Linear Space - Divide and Conquer (Hirschberg's Algorithm)
+
 import copy
 
 # Seq1 = "CACCC"
